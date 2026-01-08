c54c410e69a386abfe0279a6b8d806968eb64d96
oumai2001 <oumaimaeddahani0@gmail.com>
2026-01-08 17:02:09 +0000

    Création des exception 'exception de validation'

---
+++ exceptions/ValidationException.php
@@ -0,0 +1,8 @@
+<?php
+namespace Exceptions;
+
+class ValidationException extends \Exception {
+    public function __construct(string $message = "Validation failed", int $code = 400) {
+        parent::__construct($message, $code);
+    }
+}
